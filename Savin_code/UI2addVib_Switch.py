8ebc1ed3b40631cceccbc419fba38364a5835af7
EmptyDeck <imsejikpark@gmail.com>
2023-08-30 13:17:00 +0900

    update

--- Savin_code/UI2addVib_Switch.py
+++ Savin_code/UI2addVib_Switch.py
@@ -1,6 +1,22 @@
-selected_labels = ["door_nock","glass_shatter","car_horn","dog_bark","drilling","nothing","siren","nothing2"]
-
+import RPi.GPIO as GPIO
+from PyQt5 import QtCore, QtGui, QtWidgets
+import threading
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import QThread, pyqtSignal
+import sounddevice as sd
+import torch.nn.functional as F
+import time
+from torchvision import transforms  # Import the transforms module
+import torchaudio
+from torchvision.models import resnet18
+import torch.nn as nn
+import torch
 import os
+selected_labels = ["door_nock", "glass_shatter", "car_horn",
+                   "dog_bark", "drilling", "nothing", "siren", "nothing2"]
+
 current_path = os.getcwd()
 print("현재 경로:", current_path)
 
@@ -12,33 +28,29 @@ path_to_model = os.path.join(current_path, relative_path_to_model)
 
 print("Path to the model:", path_to_model)
 
-import torch
-import torch.nn as nn
-from torchvision.models import resnet18
 
-#One GPU or CPU
+# One GPU or CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = resnet18(pretrained=False)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(512, len(selected_labels))
 try:
     state_dict = torch.load(path_to_model, map_location=device)
-    new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+    new_state_dict = {k.replace("module.", ""): v for k,
+                      v in state_dict.items()}
     model.load_state_dict(new_state_dict)
     model = model.to(device)
     model = model.eval()
-    print("Model successfully loaded deivce : ",device)
+    print("Model successfully loaded deivce : ", device)
 except:
     print("Failed to load the model. Please check the model file.")
-    print("a")#this is here just to cause an error
-
-import torchaudio
-from torchvision import transforms  # Import the transforms module
+    print("a")  # this is here just to cause an error
 
 
-#Transform
+# Transform
 
 SAMPLE_RATE = 22050
+
 
 class MonoToColor(nn.Module):
     def __init__(self, num_channels=3):
@@ -48,6 +60,7 @@ class MonoToColor(nn.Module):
     def forward(self, tensor):
         return tensor.repeat(self.num_channels, 1, 1)
 
+
 # Apply the same transformation as used during training
 transformation = transforms.Compose([
     torchaudio.transforms.MelSpectrogram(sample_rate=SAMPLE_RATE, n_mels=128),
@@ -55,18 +68,18 @@ transformation = transforms.Compose([
     MonoToColor()
 ])
 
-import time
-import torch.nn.functional as F
-import sounddevice as sd
 
-print("device : ",device)
-## print every labels
+print("device : ", device)
+# print every labels
+
+
 def continuous_sound_prediction(model, device, transformation, sample_rate, target_sample_rate):
     # Define class labels
 
     # Record a 2 seconds mono audio at the specified sample rate
     duration = 2.0  # seconds
-    recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1) 
+    recording = sd.rec(int(duration * sample_rate),
+                       samplerate=sample_rate, channels=1)
     sd.wait()
 
     # Convert to PyTorch tensor and switch channels and frames
@@ -75,7 +88,8 @@ def continuous_sound_prediction(model, device, transformation, sample_rate, targ
 
     # Resample if necessary
     if sample_rate != target_sample_rate:
-        resampler = torchaudio.transforms.Resample(sample_rate, target_sample_rate)
+        resampler = torchaudio.transforms.Resample(
+            sample_rate, target_sample_rate)
         recording = resampler(recording)
 
     # Mix down if necessary
@@ -98,40 +112,31 @@ def continuous_sound_prediction(model, device, transformation, sample_rate, targ
     with torch.no_grad():  # deactivate autograd engine to reduce memory usage and speed up computations
         recording = recording.to(device)
         outputs = model(recording[None, ...])
-        #probabilities = F.softmax(outputs, dim=1)  # apply softmax to output (for 100%)
-        #_, predicted = torch.max(outputs, 1)
-        probabilities = torch.sigmoid(outputs)  # apply sigmoid to output (for indivisual points)
+        # probabilities = F.softmax(outputs, dim=1)  # apply softmax to output (for 100%)
+        # _, predicted = torch.max(outputs, 1)
+        # apply sigmoid to output (for indivisual points)
+        probabilities = torch.sigmoid(outputs)
         _, predicted = torch.max(outputs, 1)
 
     # Get predicted label and its corresponding probability
     predicted_label = selected_labels[predicted.item()]
-    predicted_confidence = probabilities[0, predicted.item()].item()  # get the probability of the predicted class
+    # get the probability of the predicted class
+    predicted_confidence = probabilities[0, predicted.item()].item()
 
     ######## Adjust 'x' probability   #########
     change_label = "glass_shatter"
     change_probability = 0.5
-    try:# if you have something to reduce
+    try:  # if you have something to reduce
         x_index = selected_labels.index(change_label)
-        probabilities[0, x_index] = max(0.0, probabilities[0, x_index].item() - change_probability)
-    except:#if you dont
+        probabilities[0, x_index] = max(
+            0.0, probabilities[0, x_index].item() - change_probability)
+    except:  # if you dont
         pass
     # Print the probabilities of all labels in one line
-    #prob_strs = [f"{label} {probabilities[0, idx].item():.2%}" for idx, label in enumerate(selected_labels)]
-    #print(f"/ ".join(prob_strs))
-
+    # prob_strs = [f"{label} {probabilities[0, idx].item():.2%}" for idx, label in enumerate(selected_labels)]
+    # print(f"/ ".join(prob_strs))
 
     return predicted_label, predicted_confidence, probabilities
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtGui import QMovie
-import torch
-import torch.nn as nn
-import torchaudio
-import sounddevice as sd
-import torchvision.transforms as transforms
-from torchvision.models import resnet18
-from torch.autograd import Variable
 
 
 sample_rate = SAMPLE_RATE
@@ -140,9 +145,12 @@ target_sample_rate = SAMPLE_RATE
 print(" 결과 값 : ")
 
 # Sound Analysis class running on a separate thread
+
+
 class SoundAnalysis(QThread):
     # Define a pyqtSignal with str type, which will be used to send the analysis results to the main thread
-    result_signal = pyqtSignal(str, float)  # Add a float type for the probability
+    # Add a float type for the probability
+    result_signal = pyqtSignal(str, float)
 
     def __init__(self, model, device, transformation, sample_rate):
         QThread.__init__(self)
@@ -154,9 +162,11 @@ class SoundAnalysis(QThread):
     def run(self):
         count = 0
         while True:
-            predicted_label, predicted_confidence, probabilities = continuous_sound_prediction(model, device, transformation, SAMPLE_RATE, SAMPLE_RATE)                
+            predicted_label, predicted_confidence, probabilities = continuous_sound_prediction(
+                model, device, transformation, SAMPLE_RATE, SAMPLE_RATE)
             self.result_signal.emit(predicted_label, predicted_confidence)
-            prob_strs = [f"{label} {probabilities[0, idx].item():.2%}" for idx, label in enumerate(selected_labels)]
+            prob_strs = [f"{label} {probabilities[0, idx].item():.2%}" for idx,
+                         label in enumerate(selected_labels)]
             print(f"\r{count} / " + " / ".join(prob_strs), end="")
             count = count + 1
 
@@ -177,7 +187,7 @@ class Ui_MainWindow(object):
         self.label_2.setGeometry(QtCore.QRect(30, 60, 971, 351))
         self.label_2.setText("")
         self.label_2.setObjectName("image")
-        
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 29))
@@ -191,35 +201,36 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
         # Initialize SoundAnalysis and connect the result_signal with the updateLabel function
-        self.sound_analysis = SoundAnalysis(model, device, transformation, SAMPLE_RATE)
+        self.sound_analysis = SoundAnalysis(
+            model, device, transformation, SAMPLE_RATE)
         self.sound_analysis.result_signal.connect(self.updateLabel)
         self.sound_analysis.result_signal.connect(self.updateLabel2)
         self.sound_analysis.start()  # Start the sound analysis thread
-        
+
     def updateLabel2(self, predicted_label):
-        relative_image_folder_path = "../image_file" #\ only works for windows
-        image_folder_path= os.path.join(current_path, relative_image_folder_path)
-        full_file_name = os.path.join(image_folder_path, f"{predicted_label}.gif")
+        relative_image_folder_path = "../image_file"  # \ only works for windows
+        image_folder_path = os.path.join(
+            current_path, relative_image_folder_path)
+        full_file_name = os.path.join(
+            image_folder_path, f"{predicted_label}.gif")
         self.movie = QMovie(full_file_name)
         self.label.setMovie(self.movie)
         self.movie.start()
-        #self.label_2.setPixmap(QtGui.QPixmap(full_file_name))  
-        #print(predicted_label)
+        # self.label_2.setPixmap(QtGui.QPixmap(full_file_name))
+        # print(predicted_label)
 
     def updateLabel(self, predicted_label, predicted_confidence):
-        #print("Received signal")  # Print message when signal is received
-        self.label.setText(f"{predicted_label}  {predicted_confidence*100:.2f}%")
-        #print(predicted_label)
-
+        # print("Received signal")  # Print message when signal is received
+        self.label.setText(
+            f"{predicted_label}  {predicted_confidence*100:.2f}%")
+        # print(predicted_label)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setFont(QtGui.QFont("AppleSystemUIFont",20))
+        self.label.setFont(QtGui.QFont("AppleSystemUIFont", 20))
         self.label.setStyleSheet("Color : black")
 
-import RPi.GPIO as GPIO
-import threading
 
 class VibrationController:
     def __init__(self):
@@ -228,36 +239,36 @@ class VibrationController:
         self.yellow_button_pin = 22
         self.green_button_pin = 27
         self.vibration_motor_pin = 18
-        
+
         # 진동 세기 초기화
         self.vibration_intensity_temp = 100
         self.vibration_intensity = 0
-        
+
         # 이전 스위치 상태 초기화
         self.prev_red_button_state = GPIO.HIGH
         self.prev_yellow_button_state = GPIO.HIGH
         self.prev_green_button_state = GPIO.HIGH
-        
+
         self.debounce_time = 0.2
-        
+
         # 진동 상태를 저장하는 변수
         self.vibration_on = False
-        
+
         # GPIO 초기화
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.red_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.yellow_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.green_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.vibration_motor_pin, GPIO.OUT)
-        
+
         # PWM 설정
         self.pwm_frequency = 1000
         self.pwm = GPIO.PWM(self.vibration_motor_pin, self.pwm_frequency)
         self.pwm.start(self.vibration_intensity)
-    
+
     def display_vibration_intensity(self):
         print(f"진동 세기: {self.vibration_intensity}")
-    
+
     def adjust_vibration_intensity(self, button_pin):
         if button_pin == self.red_button_pin:
             self.vibration_intensity = self.vibration_intensity_temp
@@ -265,52 +276,51 @@ class VibrationController:
             self.vibration_intensity = max(self.vibration_intensity - 10, 0)
         elif button_pin == self.green_button_pin:
             self.vibration_intensity = min(self.vibration_intensity + 10, 100)
-        
+
         self.pwm.ChangeDutyCycle(self.vibration_intensity)
         self.display_vibration_intensity()
-    
+
     def run(self):
         try:
             while True:
                 red_button_state = GPIO.input(self.red_button_pin)
                 yellow_button_state = GPIO.input(self.yellow_button_pin)
                 green_button_state = GPIO.input(self.green_button_pin)
-        
+
                 if red_button_state != self.prev_red_button_state:
                     time.sleep(self.debounce_time)
                     if red_button_state != GPIO.input(self.red_button_pin):
-                        
+
                         if not self.vibration_on:
                             self.vibration_on = True
-                            self.adjust_vibration_intensity(self.red_button_pin)
+                            self.adjust_vibration_intensity(
+                                self.red_button_pin)
                         else:
                             self.vibration_intensity_temp = self.vibration_intensity
                             self.vibration_intensity = 0
                             self.pwm.ChangeDutyCycle(self.vibration_intensity)
                             self.display_vibration_intensity()
                             self.vibration_on = False
-        
+
                     self.prev_red_button_state = red_button_state
-        
+
                 if yellow_button_state != self.prev_yellow_button_state:
                     time.sleep(self.debounce_time)
                     if yellow_button_state != GPIO.input(self.yellow_button_pin):
                         self.adjust_vibration_intensity(self.yellow_button_pin)
                     self.prev_yellow_button_state = yellow_button_state
-        
+
                 if green_button_state != self.prev_green_button_state:
                     time.sleep(self.debounce_time)
                     if green_button_state != GPIO.input(self.green_button_pin):
                         self.adjust_vibration_intensity(self.green_button_pin)
                     self.prev_green_button_state = green_button_state
-        
-                time.sleep(0.01)  
-        
+
+                time.sleep(0.01)
+
         except KeyboardInterrupt:
             self.pwm.stop()
             GPIO.cleanup()
-        
-
 
 
 if __name__ == "__main__":
